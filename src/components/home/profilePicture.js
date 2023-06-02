dca5d414639ef0aecbc5ad8a14258d7466899e71
simeon <wagnersimeon01@gmail.com>
2023-06-03 00:39:01 +0200

    Adjusting the directory structure

--- src/components/home/profilePicture.js
+++ src/components/home/profilePicture.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import "./profilePicture.css"
-import profilePicture from '../pictures/profilePictureQuadratic.jpg';
+import profilePicture from '../../assets/profilePicture.jpg';
 
 class  ProfilePicture extends Component {
     state = {  } 
---
+++ src/data/projects.js
@@ -0,0 +1,21 @@
+
+
+export const items = [
+    {
+      title: "Genetic Algorithm",
+      description:
+        "The Traveling salesman problem belongs to the NP hard problems. This means that finding the optimal solution in polynomial time is not possible. In order to achieve good results heuristic methods are used. In this project I tried to find a good solution for the problem using an evolutionary algorithm. The implementation was done in C++ as well as in Java.",
+      image: require("../posts/projects/images/genetic_algorithm.png"),
+      link: require("../App")
+    },
+    {
+      title: "Neural Network",
+      description:
+        "Neural networks are extremely fascinating. In my neural network project we leave the comfortable machine learning frameworks like PyTorch behind and take the challenge to program a neural network in Java and C. Only matrices, nothing else!",
+      image: require("../posts/projects/images/neural_network.png"),
+      link: require("../App")
+    },
+  ];
+
+
+  export default items
